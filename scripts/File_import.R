1962f6049a6543e2153314f97a7b125f6d083a99
amurali <abimur@gmail.com>
2017-12-09 02:36:44 -0800

    Update R file to read data

--- scripts/File_import.R
+++ scripts/File_import.R
@@ -5,18 +5,20 @@
 # This script reads in the data set
 
 main <- function(){
-  tables_df <- read.csv("https://raw.githubusercontent.com/PLBMR/mentalHealthDataAnalysis/master/osmiMentalHealthInTech/data/raw/osmi-survey-2016_data.csv"
-                        ,sep = ","
-                        ,stringsAsFactors = FALSE)
+  df <- readr::read_csv("https://query.data.world/s/I9KG-0l-HO-HwXVBXuiBbDfQCFxp6A");
   
   if (nrow(tables_df) > 0) {
-    #Remove file
-    file_name <- "../data/raw_data.csv"
+  
+    dir.create("../data", showWarnings = FALSE)
+    dir.create("../data/Raw_data", showWarnings = FALSE)
+    #Remove file   
+    file_name <- "../data/Raw_data/raw_data.csv"
+    
     if (file.exists(file_name)) file.remove(fn)
     
     print("Writing file to data folder as raw_data.csv")
     #Writing file to folder
-    write.csv(tables_df, file = "../data/raw_data.csv")
+    write.csv(tables_df, file = file_name)
     print("Successfully created file")
     print("Printing head of the first column of the imported file")
     print(head(tables_df[1]))
